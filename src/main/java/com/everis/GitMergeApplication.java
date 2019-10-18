fef96e553453441db8f970178782c7e6b5df4a4f
yramirec <yordan.ramirez@outlook.com>
2019-10-18 10:42:12 -0500

    first commit

---
+++ src/main/java/com/everis/GitMergeApplication.java
@@ -0,0 +1,13 @@
+package com.everis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitMergeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitMergeApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/everis/model/Parent.java
@@ -0,0 +1,5 @@
+package com.everis.model;
+
+public class Parent {
+
+}
